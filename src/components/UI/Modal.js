6bc62a6a4befd2019cd7bcd06e3325671d75a6b7
AbelardoRivas <abelardo.rivas1@gmail.com>
2023-10-08 23:10:49 -0400

    lesson 173

---
+++ src/components/UI/Modal.js
@@ -0,0 +1,31 @@
+import { Fragment } from "react";
+import classes from "./Modal.module.css";
+import ReactDOM from "react-dom";
+
+const Backdrop = (props) => {
+  return <div className={classes.backdrop}></div>;
+};
+
+const ModalOverlay = ({ children }) => {
+  return (
+    <div className={classes.modal}>
+      <div className={classes.content}>{children}</div>
+    </div>
+  );
+};
+
+const portalElement = document.getElementById("overlays");
+
+const Modal = ({ children }) => {
+  return (
+    <Fragment>
+      {ReactDOM.createPortal(<Backdrop />, portalElement)}
+      {ReactDOM.createPortal(
+        <ModalOverlay>{children}</ModalOverlay>,
+        portalElement
+      )}
+    </Fragment>
+  );
+};
+
+export default Modal;
